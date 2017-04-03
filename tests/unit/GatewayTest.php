9cfd60914d34376f41d97ad33f7bbbd9ed42edf5
Yurii Myronchuk <bladeroot@gmail.com>
2017-04-03 14:00:45 +0000

    tests

---
+++ tests/unit/GatewayTest.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * OKPAY driver for Omnipay PHP payment library
+ *
+ * @link      https://github.com/hiqdev/omnipay-okpay
+ * @package   omnipay-okpay
+ * @license   MIT
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ */
+
+namespace Omnipay\OKPAY;
+
+use Omnipay\Tests\GatewayTestCase;
+
+class GatewayTest extends GatewayTestCase
+{
+    public $gateway;
+
+    private $purse          = 'purse@company.co';
+//    private $secret         = 'sDf#$Sdf#$%';
+    private $testMode       = true;
+    private $transactionId  = 'sadf2345asf';
+    private $description    = 'Test completePurchase description';
+    private $currency       = 'USD';
+    private $amount         = '12.46';
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->gateway = new Gateway($this->getHttpClient(), $this->getHttpRequest());
+        $this->gateway->setPurse($this->purse);
+        $this->gateway->setTestMode($this->testMode);
+    }
+
+    public function testGateway()
+    {
+        $this->assertSame($this->purse,     $this->gateway->getPurse());
+        $this->assertSame($this->testMode,  $this->gateway->getTestMode());
+    }
+
+    public function testCompletePurchase()
+    {
+        $request = $this->gateway->completePurchase([
+            'transactionId' => $this->transactionId,
+        ]);
+
+        $this->assertSame($this->purse,         $request->getPurse());
+        $this->assertSame($this->testMode,      $request->getTestMode());
+        $this->assertSame($this->transactionId, $request->getTransactionId());
+    }
+
+    public function testPurchase()
+    {
+        $request = $this->gateway->purchase([
+            'transactionId' => $this->transactionId,
+            'description'   => $this->description,
+            'currency'      => $this->currency,
+            'amount'        => $this->amount,
+        ]);
+
+        $this->assertSame($this->transactionId, $request->getTransactionId());
+        $this->assertSame($this->description,   $request->getDescription());
+        $this->assertSame($this->currency,      $request->getCurrency());
+        $this->assertSame($this->amount,        $request->getAmount());
+    }
+}
---
+++ tests/unit/Message/PurchaseResponseTest.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * OKPAY driver for Omnipay PHP payment library
+ *
+ * @link      https://github.com/hiqdev/omnipay-okpay
+ * @package   omnipay-okpay
+ * @license   MIT
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ */
+
+namespace Omnipay\OKPAY\Message;
+
+use Omnipay\Tests\TestCase;
+
+class PurchaseResponseTest extends TestCase
+{
+    private $request;
+
+    private $purse          = 'ec12345';
+    private $secret         = '12()&*&+_)?><';
+    private $description    = 'Test Transaction long description';
+    private $transactionId  = '12345ASD67890sd';
+    private $amount         = '14.65';
+    private $currency       = 'USD';
+    private $testMode       = true;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->request = new PurchaseRequest($this->getHttpClient(), $this->getHttpRequest());
+        $this->request->initialize([
+            'purse'         => $this->purse,
+            'secret'        => $this->secret,
+            'description'   => $this->description,
+            'transactionId' => $this->transactionId,
+            'amount'        => $this->amount,
+            'currency'      => $this->currency,
+            'testMode'      => $this->testMode,
+        ]);
+    }
+
+    public function testSuccess()
+    {
+        $response = $this->request->send();
+
+        $this->assertFalse($response->isSuccessful());
+        $this->assertTrue($response->isRedirect());
+        $this->assertNull($response->getCode());
+        $this->assertSame('POST', $response->getRedirectMethod());
+        // $this->assertStringStartsWith('https://okpay.cc/account/mpay', $response->getRedirectUrl());
+        $this->assertSame([
+            'ok_receiver'       => $this->purse,
+            'ok_item_1_name'    => $this->description,
+            'ok_currency'       => $this->currency,
+            'ok_item_1_price'   => $this->amount,
+            'ok_invoice'        => $this->transactionId,
+        ], $response->getRedirectData());
+    }
+}
